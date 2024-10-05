be58b8811f92656c35f435fb3c8fdc605c044867
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2024-10-05 20:57:15 +0530

    Backend

--- client/src/slice/category_slice.js
+++
@@ -1,9 +0,0 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-const category_slice = createSlice({
-    name: "category",
-    initialState: "NationalPark",
-    reducers: {},
-});
-
-export default category_slice.reducer;
